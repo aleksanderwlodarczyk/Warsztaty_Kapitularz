7952de13bc9cac406c8e401075caf851171980d1
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2017-08-27 16:58:05 +0200

    skrypty na zabijanie gracza i sterowanie

---
+++ Zajecia_unity/Assets/Skrypty moje/Gracz.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gracz : MonoBehaviour {
+
+	private Vector3 startPos;
+
+	void Start () {
+		startPos = transform.position;
+	}
+	
+
+	public void Respawn(){
+		transform.position = startPos;
+	}
+}
---
+++ Zajecia_unity/Assets/Skrypty moje/KontrolowanePoruszanie.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+
+public class KontrolowanePoruszanie : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+	private float horizontal;
+	private float speedR;
+	private bool canJump;
+
+	public float speed;
+	public bool jump;
+	public float jumpForce;
+
+	void Start () {
+		rb2d = gameObject.GetComponent<Rigidbody2D> ();
+		speedR = speed;
+		canJump = true;
+	}
+
+	void Update () {
+		horizontal = Input.GetAxis ("Horizontal");
+		rb2d.AddForce (Vector2.right * horizontal * speedR);
+
+		if (horizontal * rb2d.velocity.x < 0) {
+			speedR = -speed * -2;
+		} else {
+			speedR = speed;
+		}
+
+		if (Input.GetKey (KeyCode.Space) && jump && canJump) {
+			rb2d.AddForce (Vector2.up * jumpForce);
+			canJump = false;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		if (other.gameObject.tag == "ground") {
+			canJump = true;
+		}
+	}
+}
